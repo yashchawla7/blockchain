c81853bb2beb6c075843b0550604854da66559d8
Papan Das <hexoindia@gmail.com>
2019-07-11 20:48:46 +0530

    NOde Module

--- HTML/main.js
+++ HTML/main.js
@@ -1,12 +1,90 @@
 App = {
+    baseURL: `http://${window.location.hostname}:6002`,
+    page: 'register',//register //test
+    init: function () {
+        console.log("App initialized....");
+
+        console.log('BaseURL', App.baseURL);
+
+        //App.formsJSON = JSON.parse(forms);
+        //console.log('Forms JSON', App.formsJSON)
+
+        App.loadpages();
+    },
+    loadpages: function () {
+        App.showloader(true);
+        switch (App.page) {
+            case 'test':
+                $('#container').empty();
+                $('#container').load('test.html')
+                break;
+            case 'register':
+                App.loadRegisterPage();
+                break;
+            default:
+                break;
+        }
+        App.showloader(false);
+    },
+    loadRegisterPage: function(){
+        App.showloader(false);
+
+        $('#container').empty();
+        
+        $('#container').load('register-page.html')
+    },
+    saveToBC_registerUser: function(){
+
+    },
+    loadAdminPage: function(){
+        App.showloader(false);
+
+        $('#container').empty();
+
+        $.get(App.baseURL + "/composer/admin/getAllUser", function (data, status) {
+
+            if (status === 'success') {
+
+                if (data.success) {
+                    console.log("Load", data.canvas.register.length)
+
+                }
+            }
+        }
+
+    },
+
+
+    showloader: function (param, cont = 'main') {
+        switch (cont) {
+            case 'main':
+                if (param === true) {
+                    $('#container').hide();
+                    $('#loader').show();
+                } else {
+                    $('#container').show();
+                    $('#loader').hide();
+                }
+                break;
+            case 'landing':
+                if (param === true) {
+                    $('#org-landing-container').empty();
+                    $('#org-landing-container').html('<div class="container"><h1 class="mt-5">Loading...</h1></div>');
+                } else {
+                    $('#org-landing-container').empty();
+                }
+                break;
+            default:
+                $('#container').show();
+                $('#loader').hide();
+                break;
+        }
+
+    },
 }
 
 $(function () {
     $(window).load(function () {
         App.init();
-
-        $(document).on("click", ".open-exampleModal", function(){
-            
-        });
     })
 })
--- controller/restapi/features/composer/hlcMain.js
+++ controller/restapi/features/composer/hlcMain.js
@@ -25,7 +25,7 @@ const BusinessNetworkConnection = require('composer-client').BusinessNetworkConn
 const config = require('../../../env.json');
 const NS = 'org.acme.Z2BTestNetwork';
 
-exports.addUser = function(req, res, next) {
+exports.addUser = function (req, res, next) {
     let businessNetworkConnection;
     let factory;
     businessNetworkConnection = new BusinessNetworkConnection();
@@ -33,67 +33,90 @@ exports.addUser = function(req, res, next) {
     // return businessNetworkConnection.connect(config.composer.connectionProfile, config.composer.network, config.composer.adminID, config.composer.adminPW)
     // connection in v0.15
     return businessNetworkConnection.connect(config.composer.adminCard)
-    .then(() => {
-        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
-        return businessNetworkConnection.getParticipantRegistry(NS+'.User')
-        .then(function(participantRegistry){
-            let ts = Date.now();
-            let participant = factory.newResource(NS, 'User', ts.toString());
-            participant.userId = ts.toString();
-            participant.name = 'name' + ts.toString();
-            participant.email = 'email' + ts.toString();
-            participant.phoneNumber = 'phone' + ts.toString();
-            participant.aadharNumber = 'aadhar' + ts.toString();
-            participant.IPFile = 'IP FIle' + ts.toString();
-            participant.state = 'state' + ts.toString();
-            participantRegistry.add(participant)
-            .then(() => {console.log('Successfully added'); res.send('Successfully added');})
-            .catch((error) => {console.log('Add failed',error); res.send(error);});
+        .then(() => {
+            factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+            return businessNetworkConnection.getParticipantRegistry(NS + '.User')
+                .then(function (participantRegistry) {
+                    let ts = Date.now();
+                    let participant = factory.newResource(NS, 'User', ts.toString());
+                    participant.userId = ts.toString();
+                    participant.name = 'name' + ts.toString();
+                    participant.email = 'email' + ts.toString();
+                    participant.phoneNumber = 'phone' + ts.toString();
+                    participant.aadharNumber = 'aadhar' + ts.toString();
+                    participant.IPFile = 'IP FIle' + ts.toString();
+                    participant.state = 'state' + ts.toString();
+                    participantRegistry.add(participant)
+                        .then(() => { console.log('Successfully added'); res.send('Successfully added'); })
+                        .catch((error) => { console.log('Add failed', error); res.send(error); });
 
 
-            /*return participantRegistry.get(req.body.id)
-            .then((_res) => { res.send('member already exists. add cancelled');})
-            .catch((_res) => {
-                console.log(req.body.id+' not in '+req.body.type+' registry. ');
-                let participant = factory.newResource(NS, req.body.type,req.body.id);
-                participant.userId = req.body.userId;
-                participant.name = req.body.name;
-                participant.email = req.body.email;
-                participant.phoneNumber = req.body.phoneNumber;
-                participant.aadharNumber = req.body.aadharNumber;
-                participant.IPFile = req.body.IPFile;
-                participant.state = req.body.state;
-                participantRegistry.add(participant)
-                .then(() => {console.log(req.body.companyName+' successfully added'); res.send(req.body.companyName+' successfully added');})
-                .catch((error) => {console.log(req.body.companyName+' add failed',error); res.send(error);});
-            });*/
+                    /*return participantRegistry.get(req.body.id)
+                    .then((_res) => { res.send('member already exists. add cancelled');})
+                    .catch((_res) => {
+                        console.log(req.body.id+' not in '+req.body.type+' registry. ');
+                        let participant = factory.newResource(NS, req.body.type,req.body.id);
+                        participant.userId = req.body.userId;
+                        participant.name = req.body.name;
+                        participant.email = req.body.email;
+                        participant.phoneNumber = req.body.phoneNumber;
+                        participant.aadharNumber = req.body.aadharNumber;
+                        participant.IPFile = req.body.IPFile;
+                        participant.state = req.body.state;
+                        participantRegistry.add(participant)
+                        .then(() => {console.log(req.body.companyName+' successfully added'); res.send(req.body.companyName+' successfully added');})
+                        .catch((error) => {console.log(req.body.companyName+' add failed',error); res.send(error);});
+                    });*/
+                })
+                .catch((error) => { console.log('error with getParticipantRegistry', error); res.send(error); });
         })
-        .catch((error) => {console.log('error with getParticipantRegistry', error); res.send(error);});
-    })
-    .catch((error) => {console.log('error with businessNetworkConnection', error); res.send(error);});
+        .catch((error) => { console.log('error with businessNetworkConnection', error); res.send(error); });
 };
 
 
-exports.getAllUser = function(req, res, next) {
+exports.getAllUser = function (req, res, next) {
     let businessNetworkConnection;
     let factory;
+    let allUser = new Array();
     businessNetworkConnection = new BusinessNetworkConnection();
     // connection prior to V0.15
     // return businessNetworkConnection.connect(config.composer.connectionProfile, config.composer.network, config.composer.adminID, config.composer.adminPW)
     // connection in v0.15
-    return businessNetworkConnection.connect(config.composer.adminCard)
-    .then(() => {
-        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
-        return businessNetworkConnection.getParticipantRegistry(NS+'.User')
-        .then(function(participantRegistry){
-            
-            return participantRegistry.getAll()
-            .then((_res) => { res.send('member already exists. add cancelled'); console.log(_res) })
-            .catch((_res) => {
-                console.log('No one registered', _res);
-            });
-        })
-        .catch((error) => {console.log('error with getParticipantRegistry', error); res.send(error);});
-    })
-    .catch((error) => {console.log('error with businessNetworkConnection', error); res.send(error);});
+
+    let serializer;
+    let archiveFile = fs.readFileSync(path.join(path.dirname(require.main.filename), 'network', 'dist', 'zerotoblockchain-network.bna'));
+    return BusinessNetworkDefinition.fromArchive(archiveFile)
+        .then((bnd) => {
+            serializer = bnd.getSerializer();
+
+            return businessNetworkConnection.connect(config.composer.adminCard)
+                .then(() => {
+                    factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+                    return businessNetworkConnection.getParticipantRegistry(NS + '.User')
+                        .then(function (participantRegistry) {
+
+                            return participantRegistry.getAll()
+                                .then((members) => {
+                                    console.log('There are ' + members.length + ' entries.');
+
+                                    for (let each in members) {
+                                        (function (_idx, _arr) {
+                                            let _jsn = serializer.toJSON(_arr[_idx]);
+                                            console.log(_idx, _jsn)
+                                            allUser.push(_jsn);
+                                        })(each, members)
+                                    }
+
+                                    res.send({ 'result': 'success', 'success': true, 'user': allUser });
+                                })
+                                .catch((_res) => {
+                                    console.log('No one registered', _res);
+                                });
+                        })
+                        .catch((error) => { console.log('error with getParticipantRegistry', error); res.send(error); });
+                })
+                .catch((error) => { console.log('error with businessNetworkConnection', error); res.send(error); });
+        }
+
+    
 };
